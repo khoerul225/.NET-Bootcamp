1736d63bdea1c55b7d9ba7fed8c32f7867326427
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-08-30 13:41:44 +0700

    selesai

---
+++ ConsoleApp1/IEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartTwo
+{
+    internal interface IEmployee
+    {
+        public List<Employee> InitDataEmployee();
+        
+        public void Showlist<T> (ref List<T> list);  ///Generic Show List
+
+        public Employee FindEmployeeById(List <Employee> list, int id);
+
+        public decimal GetTotalSalary<T> (ref List<T> list);  ///Generic Total
+
+        public List<Employee> FindListRange (List<Employee> list,decimal startForm,decimal endTo);
+
+    }
+}
---
+++ Latihan Lagi/NewFolder/Latihan Soal Quiz CSharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan_Lagi.NewFolder
+{
+    internal class Latihan_Soal_Quiz_CSharp
+    {
+    }
+}
