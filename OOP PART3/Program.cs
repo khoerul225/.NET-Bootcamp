f94012cce698b8bae899ba3e4ab914d74c56e017
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-08-31 16:22:32 +0700

    final project

--- OOP PART3/Program.cs
+++ OOP PART3/Program.cs
@@ -11,7 +11,7 @@ Person yuli = new Customer ("Yuli", "Ayu", "yuliAyu@gmail.com", new DateTime(199
 Person widi = new Customer ("Widi", "Amalia", "widiAmalia@gmail.com", new DateTime(1998,09,30),"110-111");
 Person rini = new Customer ("Rini", "Marlina", "riniMarlina@gmail.com", new DateTime(1998,08,04),"110-112");
 
-
+//Suber data
 var listOfCustomer = new List<Person> {yuli,widi,rini};
 
 var incomeYuli = new Dictionary<string, decimal> 
@@ -49,4 +49,54 @@ foreach (var item in listOfCustomer)
     WriteLine(item);
 }
 
-LinQ.IntroLinQ();
+
+
+
+WriteLine("================================Revenue Highest==================================================");
+var listRange = listOfCustomer.Where(x => x.TotalRevenue < 300_000_000).ToList();
+foreach (var item in listRange)
+{
+    WriteLine(item.ToString());
+}
+
+WriteLine();
+
+var listMin = listOfCustomer.Min(x => x.TotalRevenue);
+WriteLine($"Cust Min Revenue : {listMin}");
+
+var listMax = listOfCustomer.Max(x => x.TotalRevenue);
+WriteLine($"Cust Max Revenue : {listMax}");
+
+
+
+var listThenMax = listOfCustomer.Where(x => x.TotalRevenue<listMax);
+WriteLine("================================Revenue LowerThen Highest==================================================");
+foreach (var item in listThenMax)
+{
+    WriteLine(item.ToString());
+}
+
+//Construktor adalah epenghubung kesemua Class
+WriteLine("================================Select==================================================");
+var query = listOfCustomer.Select
+(cust=>
+    new
+    {
+        FullName = cust.FirstName + " " + cust.LastName,
+        Email = cust.Email,
+        TotalRevenue = cust.TotalRevenue
+    }
+);
+
+/// Kurung Biasa
+
+foreach (var item in query)
+{
+    WriteLine(item);
+}
+
+
+
+
+
+/*LinQ.IntroLinQ();*/
